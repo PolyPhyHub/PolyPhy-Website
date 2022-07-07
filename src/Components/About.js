7a91a2c65141de8fdc66de59da1adfe5474b6451
Asavari Ambavane <ambavaneasavari@gmail.com>
2022-07-07 09:41:19 +0530

    loader

--- src/Components/About.js
+++ src/Components/About.js
@@ -34,12 +34,7 @@ const About = () => {
         optimal transport network given a set of point data in 3D space. Such
         data can represent the distribution of galaxies or dark matter halos,
         typically on the scales of 100s of megaparsecs. MCPM uses a swarm of
-        millions of particle-like agents to explore the simulation domain. These
-        agents are attracted to each other, as well as the input data. After a
-        number of iterations, the simulation reaches a dynamic equilibrium: the
-        agents still move, but the totality of their trajectories is stable. The
-        output of the algorithm at this stage is a 3D spatio-temporal density of
-        the agents which we interpret as an estimate of the Cosmic web.
+        millions of particle-like agents to explore the simulation domain.
       </p>
       <br />
       <p>
