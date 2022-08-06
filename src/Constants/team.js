983a38afaa9a0103e34add990dfe25afa5c2acf2
Asavari Ambavane <ambavaneasavari@gmail.com>
2022-08-06 18:09:06 +0530

    team

---
+++ src/Constants/team.js
@@ -0,0 +1,38 @@
+import oskar from "../Assets/oskar_elek.jpg";
+import joseph from "../Assets/joseph.jpeg";
+import carlos from "../Assets/carlos.jfif";
+import angus from "../Assets/angus.jfif";
+
+export const team = [
+    {
+        name: "Oskar Elek",
+        about: "Primary author, computational media researcher at Creative Coding lab, UC Santa Cruz",
+        image: oskar
+    },
+    {
+        name: "Joseph N. Burchett",
+        about: "Astronomer at UC Santa Cruz, expert in galactic ecosystems",
+        image: joseph
+    },
+    
+    {
+        name: "Carlos Maltzahn",
+        about: "Founder and Director of the UC Santa Cruz Center for Research in Open Source Software (CROSS)",
+        image: carlos
+    },
+    {
+        name: "Angus G. Forbes",
+        about: "Director of Creative Coding lab, UC Santa Cruz",
+        image: angus
+    },
+]
+
+export const sig_contri = [
+    {name: "J. Xavier Prochaska"},
+    {name: "Jan Ivanecky"},
+    {name: "Sage Jenson"},
+    {name: "Daisuke Nagai"},
+    {name: "Sebastian Herholz"},
+    {name: "Jaroslav Křivánek"},
+    {name: "Drew Ehrlich"},
+]
